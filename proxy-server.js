202cd63a7f283bc4471ad45154628ee4922383f9
JunXin02 <95616470+JunXin02@users.noreply.github.com>
2025-10-15 17:59:16 +0600

    Update proxy-server.js

--- proxy-server.js
+++ proxy-server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Final base
-const proxyHost = 'https://live.dinesh29.com.np';
+const proxyHost = 'https://d15.epicquesthero.com:999';
 
 const proxyAgent = new SocksProxyAgent(
   'socks5h://@bdiix_bypass:@bdiix_bypass@circle.bypassempire.com:1080'
@@ -16,7 +16,7 @@ const proxyAgent = new SocksProxyAgent(
 app.use('/jiotv/:id/stream_0.m3u8', (req, res, next) => {
   const { id } = req.params;
 
-  const target = `${proxyHost}/stream/jiotv/${id}/stream_0.m3u8`;
+  const target = `${proxyHost}/hls/${id}.m3u8?md5=WU2BMsOyX10ZIIe1Ck8uBg&expires=1760536410`;
 
   return createProxyMiddleware({
     target,
@@ -25,12 +25,13 @@ app.use('/jiotv/:id/stream_0.m3u8', (req, res, next) => {
     selfHandleResponse: false,
     pathRewrite: () => '', // no rewrite needed
     onProxyReq: (proxyReq) => {
-      proxyReq.setHeader('Origin', proxyHost);
+      proxyReq.setHeader('Origin', 'https://stylisheleg4nt.com');
+      proxyReq.setHeader('referer', 'https://stylisheleg4nt.com');
       proxyReq.setHeader('User-Agent', 'Mozilla/5.0');
     },
   })(req, res, next);
 });
 
 app.listen(PORT, () => {
-  console.log(`✅ Proxy live: http://localhost:${PORT}/jiotv/{id}/stream_0.m3u8`);
+  console.log(`✅ Proxy live: http://localhost:${PORT}/hls/{id}.m3u8?md5=WU2BMsOyX10ZIIe1Ck8uBg&expires=1760536410`);
 });
